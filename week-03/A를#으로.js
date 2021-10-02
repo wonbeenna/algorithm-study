97b4caf2fd5e0f8600673ee5d04a1dc727f8b8bf
wonbeenna <nwbnwb@naver.com>
2021-10-02 19:16:54 +0900

    week03

---
+++ week-03/A를#으로.js
@@ -0,0 +1,21 @@
+// 대문자로 이루어진 영어단어가 입력되면 단어에 포함된 ‘A'를 모두 ’#‘으로 바꾸어 출력하는 프로그램을 작성하세요.
+// ▣ 입력설명
+// 첫 번째 줄에 문자열이 입력된다.
+// ▣ 출력설명
+// 첫 번째 줄에 바뀐 단어를 출력한다.
+// ▣ 입력예제  BANANA
+// ▣ 출력예제  B#N#N#
+
+function solution(s) {
+  let result = "";
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "A") {
+      result += "#";
+    } else {
+      result += s[i];
+    }
+  }
+  return result;
+}
+console.log(solution("BANANA"));
