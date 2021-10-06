3d97680caa563bc60689747405812217293255d0
wonbeenna <nwbnwb@naver.com>
2021-10-06 22:41:44 +0900

    week04

---
+++ week-04/가운데문자출력.js
@@ -0,0 +1,24 @@
+// 소문자로 된 단어(문자열)가 입력되면 그 단어의 가운데 문자를 출력하는 프로그램을 작성하세 요. 단 단어의 길이가 짝수일 경우 가운데 2개의 문자를 출력합니다.
+// ▣ 입력설명
+// 첫 줄에 문자열이 입력된다. 문자열의 길이는 100을 넘지 않습니다.
+// ▣ 출력설명
+// 첫 줄에 가운데 문자를 출력합니다.
+// ▣ 입력예제  study
+// ▣ 출력예제  u
+// ▣ 입력예제  good
+// ▣ 출력예제  oo
+
+function solution(str) {
+  let result;
+
+  let mid = Math.floor(str.length / 2);
+  if (str.length % 2 !== 0) {
+    result = str.substring(mid, mid + 1);
+  } else {
+    result = str.substring(mid - 1, mid + 1);
+  }
+
+  return result;
+}
+
+console.log(solution("good"));
