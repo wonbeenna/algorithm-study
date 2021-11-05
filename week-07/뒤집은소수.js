d804ff2a798ca624b8ee9b73af552d3a898bb5cf
wonbeenna <nwbnwb@naver.com>
2021-11-06 06:13:37 +0900

    week07

---
+++ week-07/뒤집은소수.js
@@ -0,0 +1,45 @@
+// N개의 자연수가 입력되면 각 자연수를 뒤집은 후 그 뒤집은 수가 소수이면 그 소수를 출력하 는 프로그램을 작성하세요. 예를 들어 32를 뒤집으면 23이고, 23은 소수이다. 그러면 23을 출 력한다. 단 910를 뒤집으면 19로 숫자화 해야 한다. 첫 자리부터의 연속된 0은 무시한다.
+// ▣ 입력설명
+// 첫 줄에 자연수의 개수 N(3<=N<=100)이 주어지고, 그 다음 줄에 N개의 자연수가 주어진다. 각 자연수의 크기는 100,000를 넘지 않는다.
+// ▣ 출력설명
+// 첫 줄에 뒤집은 소수를 출력합니다. 출력순서는 입력된 순서대로 출력합니다.
+// ▣ 입력예제
+// 9
+// 32 55 62 20 250 370 200 30 100
+// ▣ 출력예제 23 2 73 2 3
+
+// 소수만들기
+function isPrime(num) {
+  if (num === 1) {
+    return false;
+  }
+  for (let i = 2; i <= parseInt(Math.sqrt(num)); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function solution(arr) {
+  let answer = [];
+
+  for (let x of arr) {
+    // res에 reverse값 넣음
+    let res = 0;
+
+    // reverse
+    while (x) {
+      let t = x % 10; // x의 1의 자리
+      res = res * 10 + t;
+      x = parseInt(x / 10); // 1의자리를 제외한 앞에 수
+    }
+
+    if (isPrime(res)) {
+      answer.push(res);
+    }
+  }
+  return answer;
+}
+
+console.log(solution([32, 55, 62, 20, 250, 370, 200, 30, 100]));
