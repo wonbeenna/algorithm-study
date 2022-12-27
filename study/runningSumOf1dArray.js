618f167d410bff9b0e97be9e95bc7044915a8b35
wonbeenna <nwbnwb@naver.com>
2022-12-28 01:21:50 +0900

    1480

---
+++ study/runningSumOf1dArray.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+    let result = [nums[0]];
+
+    for(let i = 1; i < nums.length; i++){
+        result.push(nums[i] += nums[i - 1])
+    };
+
+    return result;
+};
